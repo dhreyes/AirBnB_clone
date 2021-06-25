ac71a5f38b46e0639aa70190380f59ab215bc8ec
gavazcal <2392@holbertonschool.com>
2021-06-25 16:58:21 +0000

    adding empty classes for task 9

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""amenity class module"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """empty amenity class"""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City class module"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """empty city class instance"""
+    state_id = ""
+    name = ""
